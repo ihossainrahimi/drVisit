462f1c0746e8aba486e7006528c85d9774c55c15
Alireza Shahmoradi <alirezashahmoradi98@gmail.com>
2024-08-04 01:26:01 +0330

    open login modal on register click

--- src/components/MenuDrawer/index.tsx
+++ src/components/MenuDrawer/index.tsx
@@ -10,81 +10,96 @@ import {
   Typography
 } from '@mui/material';
 import Link from 'next/link';
+import { useState } from 'react';
 
 import { colorPalette } from '@/constants/colorPalette';
 import { websiteUrls } from '@/constants/urls';
 import { useAppSelector } from '@/store';
 
 import { Drawer } from '../Header/components/Drawer';
+import { LoginModal } from '../LoginModal';
 import classes from './index.module.scss';
 import { MenuDrawerProps } from './models';
 
 export const MenuDrawer = ({ isOpen, onClose }: MenuDrawerProps) => {
   const userData = useAppSelector((state) => state.userData);
+  const [isOpenLoginModal, setIsOpenLoginModal] = useState(false);
+
+  const handleOpenLoginModal = () => {
+    setIsOpenLoginModal(true);
+  };
+
+  const handleCloseLoginModal = () => {
+    setIsOpenLoginModal(false);
+  };
 
   return (
-    <Drawer dir='rtl' open={isOpen} onClose={onClose}>
-      <div className={classes.root}>
-        <Grid container spacing={8}>
-          <Grid item xs={8}>
-            <Button startIcon={<Favorite color='error' />}>
-              <Link href={websiteUrls.home}>
-                <Typography color={colorPalette.main} variant='h4'>
-                  دکتردکتر
+    <>
+      <LoginModal isOpen={isOpenLoginModal} onClose={handleCloseLoginModal} />
+      <Drawer dir='rtl' open={isOpen} onClose={onClose}>
+        <div className={classes.root}>
+          <Grid container spacing={8}>
+            <Grid item xs={8}>
+              <Button startIcon={<Favorite color='error' />}>
+                <Link href={websiteUrls.home}>
+                  <Typography color={colorPalette.main} variant='h4'>
+                    دکتردکتر
+                  </Typography>
+                </Link>
+              </Button>
+            </Grid>
+            <Grid item xs={4}>
+              <IconButton onClick={onClose}>
+                <Close />
+              </IconButton>
+            </Grid>
+          </Grid>
+          {userData.data.id ? (
+            <Link href={websiteUrls.patientProfile}>
+              <Button
+                className={classes.login_button}
+                fullWidth
+                variant='outlined'
+                startIcon={<PersonOutlined />}
+              >
+                <Typography variant='button' color={colorPalette.main}>
+                  {userData.data.username}
                 </Typography>
-              </Link>
-            </Button>
-          </Grid>
-          <Grid item xs={4}>
-            <IconButton onClick={onClose}>
-              <Close />
-            </IconButton>
-          </Grid>
-        </Grid>
-        {userData.data.id ? (
-          <Link href={websiteUrls.patientProfile}>
+              </Button>
+            </Link>
+          ) : (
             <Button
               className={classes.login_button}
               fullWidth
               variant='outlined'
               startIcon={<PersonOutlined />}
+              onClick={handleOpenLoginModal}
             >
               <Typography variant='button' color={colorPalette.main}>
-                {userData.data.username}
+                ورود / عضویت
               </Typography>
             </Button>
-          </Link>
-        ) : (
-          <Button
-            className={classes.login_button}
-            fullWidth
-            variant='outlined'
-            startIcon={<PersonOutlined />}
-          >
-            <Typography variant='button' color={colorPalette.main}>
-              ورود / عضویت
-            </Typography>
-          </Button>
-        )}
-        <div className={classes.navigation_items_container}>
-          <Link href={websiteUrls.expertiseSearch}>
-            <Typography variant='body2'>نوبت دهی</Typography>
-          </Link>
-          <Divider className={classes.divider} />
-          <Accordion>
-            <AccordionSummary expandIcon={<ExpandMore />}>
-              <Typography variant='body2'>تخصص ها</Typography>
-            </AccordionSummary>
-            <AccordionDetails></AccordionDetails>
-          </Accordion>
-          <Divider className={classes.divider} />
-          <Link href={websiteUrls.drLogin}>
-            <Typography variant='body2' color={colorPalette.secondary}>
-              برای پزشکان
-            </Typography>
-          </Link>
+          )}
+          <div className={classes.navigation_items_container}>
+            <Link href={websiteUrls.expertiseSearch}>
+              <Typography variant='body2'>نوبت دهی</Typography>
+            </Link>
+            <Divider className={classes.divider} />
+            <Accordion>
+              <AccordionSummary expandIcon={<ExpandMore />}>
+                <Typography variant='body2'>تخصص ها</Typography>
+              </AccordionSummary>
+              <AccordionDetails></AccordionDetails>
+            </Accordion>
+            <Divider className={classes.divider} />
+            <Link href={websiteUrls.drLogin}>
+              <Typography variant='body2' color={colorPalette.secondary}>
+                برای پزشکان
+              </Typography>
+            </Link>
+          </div>
         </div>
-      </div>
-    </Drawer>
+      </Drawer>
+    </>
   );
 };
